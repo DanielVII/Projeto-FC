4b02031b185a044807f2a69bed48d64498351ca1
DanielVII <62727519+DanielVII@users.noreply.github.com>
2021-02-02 19:50:57 -0300

    Limpando código
    Mudanças para deixar o código mais compreensível,

--- Código.py/Telas.py
+++ Código.py/Telas.py
@@ -1,6 +1,6 @@
 # Author: DanielVii
 
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
 import mysql.connector as msc
 
 
@@ -14,42 +14,61 @@ class Telas:
         self.__nome_janela = 'Projeto Fc'
 
         # Genericos do layout
-        self.__layout = list()
+        self.__titulo = str()
+        self.__body_text = list()
+        self.__espaco_entre_titulo_e_body = int()
+        self.__body = dict()
 
-        # Generico. Coletar dados
+        self.__body_standard()
+
+        # Genericos Coletar dados
         self.__table_name = str()
 
-
     def boas_vindas(self):
-        self.__layout = [
-            [sg.Text('\n')],
-            [sg.Text(f'{"Seja bem-vindo(a) ao Projeto Facilite!":^45}', font=self.__tipo_fonte)],
-            [sg.Text('\n'*3)],
-            [sg.Text('Estamos aqui para facilitar o começo de sua \nempresa e aumentar o lucro de suas vendas.',
-                     font=self.__tipo_fonte)],
-            [sg.Text('\n'*3)],
-            [sg.Text(f'Vamos começar!', font=self.__tipo_fonte)]
+
+        self.__titulo = 'Seja bem-vindo(a) ao Projeto Facilite!'
+
+        self.__espaco_entre_titulo_e_body = 2
+
+        self.__body_text = [
+            'Estamos aqui para facilitar o começo de sua',
+            'empresa e aumentar o lucro de suas vendas.',
+            '',
+            '',
+            '',
+            'Vamos começar!'
         ]
+
         self.__tela_generica()
 
     def entregar_informacoes_sobre_o_que_serah_requisitato(self):
-        self.__layout = [
-            [sg.Text('\n'*2)],
-            [sg.Text('Para começar, \nprecisaremos de algumas informações:', font=self.__tipo_fonte)],
-            [sg.Text('\n')],
-            [sg.Text('- O custo para fazer a unidade \n  do seu produto.', font=self.__tipo_fonte)],
-            [sg.Text('- O tanto de dinheiro que sua empresa tem,\n  ou seja, seu capital.', font=self.__tipo_fonte)],
-            [sg.Text('- O custo fixo mensal de sua empresa.', font=self.__tipo_fonte)],
-            [sg.Text('\n'*1)]
+
+        self.__titulo = 'Para começar'
+
+        self.__espaco_entre_titulo_e_body = 1
+
+        self.__body_text = [
+            'precisaremos de algumas informações: ',
+            '',
+            '- O custo para fazer a unidade do seu produto.',
+            '- O tanto de dinheiro que sua empresa tem,',
+            '  ou seja, seu capital.',
+            '- O custo fixo mensal de sua empresa.'
         ]
+
         self.__tela_generica()
 
     def capital_insuficiente(self):
-        self.__layout = [
-            [sg.Text('\n'*3)],
-            [sg.Text('O seu capital é insuficiente para começar a empresa. :(')],
-            [sg.Text('\n'*11)]
+
+        self.__titulo = 'O seu capital é insuficiente.'
+
+        self.__espaco_entre_titulo_e_body = 2
+
+        self.__body_text = [
+            '- Com o seu capital atual não é possível ',
+            '  se sustentar e fabricar um produto.'
         ]
+
         self.__tela_generica()
 
     def resultado_inicial(self):
@@ -64,39 +83,39 @@ class Telas:
 
         preco_medio = ultima_linha[2]
 
-        self.__layout = [
-            [sg.Text('\n')],
-            [sg.Text(f'{"Informações para começar":^45}', font=self.__tipo_fonte)],
-            [sg.Text('\n')],
-            [sg.Text(f'- Com o seu capital é possivel produzir', font=self.__tipo_fonte)],
-            [sg.Text(f'  {capacidade_produ} unidades.', font=self.__tipo_fonte)],
-            [sg.Text('- Cada unidade terá o custo de produção de', font=self.__tipo_fonte)],
-            [sg.Text(f'  R$ {preco_medio:.2f}.', font=self.__tipo_fonte)],
-            [sg.Text('  Levando em consideração todos os seus custos.', font=self.__tipo_fonte)],
-            [sg.Text('\n')]
-        ]
+        self.__titulo = "Informações para começar"
+
+        self.__espaco_entre_titulo_e_body = 1
+
+        self.__body_text = ['- Com o seu capital é possivel produzir',
+                            f'  {capacidade_produ} unidades.',
+                            '- Cada unidade terá o custo de produção de',
+                            f'  R$ {preco_medio:.2f}.',
+                            '  Levando em consideração todos os seus custos.'
+                            ]
+
         self.__tela_generica()
 
     def hora_de_coletar(self):
-        self.__layout = [
-            [sg.Text('\n')],
-            [sg.Text(f'{"Hora de entrar em ação!":^45}', font=self.__tipo_fonte)],
-            [sg.Text('\n')],
-            [sg.Text('- Para aprimorarmos o desempenho de suas vendas', font=self.__tipo_fonte)],
-            [sg.Text('  vamos precisar de mais informações.', font=self.__tipo_fonte)],
-            [sg.Text('  Elas são: a demanda mensal e o preço unitario,', font=self.__tipo_fonte)],
-            [sg.Text('  dos ultimos 3 meses. ', font=self.__tipo_fonte)],
-            [sg.Text('\n'*3)]
+        self.__titulo = 'Hora de entrar em ação!'
+
+        self.__espaco_entre_titulo_e_body = 1
+
+        self.__body_text = [
+            '- Para aprimorarmos o desempenho de suas vendas',
+            '  vamos precisar de mais informações.',
+            '',
+            'Elas são:',
+            '- Demanda mensal, dos ultimos 3 meses.',
+            '- preço da unidade em cada mês'
         ]
 
         self.__tela_generica()
 
     def valores_incosistentes(self):
-        self.__layout = [
-            [sg.Text('\n' * 3)],
-            [sg.Text('Os valores dados são inconsistentes. Tente novamente. ')],
-            [sg.Text('\n' * 11)]
-        ]
+
+        self.__titulo = 'Os valores dados são inconsistentes.'
+
         self.__tela_generica()
 
     def valores_ideais(self):
@@ -113,18 +132,20 @@ class Telas:
         raiz_dois = ultima_linha[4]
         lucro_ideal = ultima_linha[5]
 
-        self.__layout = [
-            [sg.Text('\n')],
-            [sg.Text(f'{"Aprimorando as vendas":^45}', font=self.__tipo_fonte)],
-            [sg.Text('\n')],
-            [sg.Text('- Você terá lucro quando o preço do produto', font=self.__tipo_fonte)],
-            [sg.Text(f'  for maior que R$ {raiz_um:.2f} e menor que R$ {raiz_dois:.2f}', font=self.__tipo_fonte)],
-            [sg.Text('- O seu maior lucro ocorrerá quando o produto', font=self.__tipo_fonte)],
-            [sg.Text(f'  estiver ao preço de R$ {preco_ideal:.2f}. Será um lucro', font=self.__tipo_fonte)],
-            [sg.Text(f'  de: R$ {lucro_ideal:.2f}', font=self.__tipo_fonte)],
-            [sg.Text(f'- Será necessário um estoque de {estoque_nec} unidades,', font=self.__tipo_fonte)],
-            [sg.Text('  caso sua escolha seja pelo lucro máximo.', font=self.__tipo_fonte)]
+        self.__titulo = "Aprimorando as vendas"
+
+        self.__espaco_entre_titulo_e_body = 1
+
+        self.__body_text = [
+            '- Você terá lucro quando o preço do produto',
+            f'  for maior que R$ {raiz_um:.2f} e menor que R$ {raiz_dois:.2f}',
+            '- O seu maior lucro ocorrerá quando o produto',
+            f'  estiver ao preço de R$ {preco_ideal:.2f}. Será um lucro',
+            f'  de: R$ {lucro_ideal:.2f}',
+            f'- Será necessário um estoque de {estoque_nec} unidades,',
+            '  caso sua escolha seja pelo lucro máximo.'
         ]
+
         self.__tela_generica()
 
     def simulacao_lucro(self):
@@ -139,32 +160,79 @@ class Telas:
         estoque_nec = ultima_linha[2]
         possivel_lucro = ultima_linha[3]
 
-        self.__layout = [
-            [sg.Text('\n')],
-            [sg.Text(f'{"Simulando futuros lucros":^45}', font=self.__tipo_fonte)],
-            [sg.Text('\n')],
-            [sg.Text(f'- Você terá um lucro de R$ {possivel_lucro:.2f}, quando o', font=self.__tipo_fonte)],
-            [sg.Text(f'  seu produto estiver ao preço de R$ {valor_dado:.2f}.', font=self.__tipo_fonte)],
-            [sg.Text(f'- Será necessário um estoque de {estoque_nec} unidades', font=self.__tipo_fonte)],
-            [sg.Text('  para suprir a demanda.', font=self.__tipo_fonte)],
-            [sg.Text('\n'*4)]
+        self.__titulo = 'Simulando futuros lucros'
+
+        self.__espaco_entre_titulo_e_body = 1
+
+        self.__body_text = [
+            f'- Você terá um lucro de R$ {possivel_lucro:.2f}, quando o',
+            f'  seu produto estiver ao preço de R$ {valor_dado:.2f}.',
+            f'- Será necessário um estoque de {estoque_nec} unidades',
+            '  para suprir a demanda.'
         ]
 
         self.__tela_generica()
 
     def __tela_generica(self):
-        layout_botoes = [
-            [sg.Submit("Continuar", size=(8, 1)), sg.Cancel("cancelar", size=(8, 1))]
+
+        self.__colocar_texto_no_body()
+
+        botoes = [Sg.Submit("Continuar", size=(8, 1)), Sg.Cancel("cancelar", size=(8, 1))]
+
+        layout = [
+            [Sg.Text('\n')],
+            [Sg.Text(f'{self.__titulo:^45}', font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_1'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_2'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_3'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_4'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_5'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_6'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_7'], font=self.__tipo_fonte)],
+            [Sg.Text(self.__body['parte_8'], font=self.__tipo_fonte)],
+            botoes
         ]
-        self.__layout.extend(layout_botoes)
-        janela = sg.Window(self.__nome_janela, size=self.__tamanho_janela).layout(self.__layout)
+
+        janela = Sg.Window(self.__nome_janela, size=self.__tamanho_janela).layout(layout)
 
         botao, ___ = janela.read()
 
         if botao == 'cancelar':
             self.continuar = False
 
+        self.__clean()
         janela.close()
+
+    def __body_standard(self):
+        for parte in range(1, 9):
+            self.__body[f'parte_{parte}'] = ''
+
+    def __colocar_texto_no_body(self):
+        part_to_start = 1 + self.__espaco_entre_titulo_e_body
+
+        posicao_do_texto_dado = 0
+        for parte in range(part_to_start, 9):
+            try:
+                self.__body[f'parte_{parte}'] = self.__body_text[posicao_do_texto_dado]
+
+                posicao_do_texto_dado += 1
+            except IndexError:
+                break
+
+    def __clean(self):
+        self.__titulo = ''
+        self.__espaco_entre_titulo_e_body = 0
+        self.__body_text = [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            ''
+        ]
+        self.__colocar_texto_no_body()
 
     def __pegar_informacao_do_bd(self):
         """
@@ -186,3 +254,4 @@ class Telas:
             mydb.close()
 
             return resultado
+
